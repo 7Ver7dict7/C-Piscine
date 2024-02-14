c86d20f5cc07f54b547cff69b0cfdf97f7f284a7
infectedUser68 <romansalmhofer@gmail.com>
2024-02-14 17:01:37 +0000

    me muscles

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_print_alphabet(void)
+{
+	int i= 97;
+	while (i >= 97 && i <= 122){
+		char c = i;
+		write(1, &c, 1);
+		i++;
+	}
+}
+/*
+ *void ft_print_alphabet(void)
+ {
+	int i;	
+ 	for (i = 97 , i<123 , i++){
+		char c = i;
+		write(1, &c, 1);
+	}	
+ }
+ *
+ *
+ *
+ */
+
+int main(){
+	ft_print_alphabet();
+}
+
